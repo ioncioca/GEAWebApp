47acc7a5a884efd8d81964dcacc46d2f9480c305
Cioca <Ion.Cioca@gea.com>
2023-12-15 22:16:53 +0100

    changes to import statements

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import db from './db'; // Import the database connection
 import cors from 'cors';
-import mysql from 'mysql';
+import * as mysql from 'mysql2';
 
 
 const app = express();
